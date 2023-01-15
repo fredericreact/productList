a35329e8afaec41b192d90909e1066fa1ca50d6c
unknown <fredericreact@gmail.com>
2023-01-15 18:12:34 +0800

    Toggling Favorites with the Context API

--- src/context/products-context.js
+++ src/context/products-context.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 
-export const ProductsContext = React.createContext({products: []})
+export const ProductsContext = React.createContext({
+    products: [],
+toggleFav: (id) =>{}
+})
 
 export default props => {
     const [productsList, setProductsList] = useState([
@@ -28,10 +31,26 @@ export default props => {
             description: 'Street style! An orange hat.',
             isFavorite: false
           }
-    ])
+    ]);
+
+    const toggleFavorite = productId => {
+        setProductsList(currentProdList =>{
+            const prodIndex = currentProdList.findIndex(
+                p => p.id === productId
+              );
+              const newFavStatus = !currentProdList[prodIndex].isFavorite;
+              const updatedProducts = [...currentProdList];
+              updatedProducts[prodIndex] = {
+                ...currentProdList[prodIndex],
+                isFavorite: newFavStatus
+              };
+            return updatedProducts;
+        })
+    }
+
     return (
         <ProductsContext.Provider value={
-            {products: productsList}
+            {products: productsList, toggleFav: toggleFavorite}
         }>
             {props.children}
         </ProductsContext.Provider>
